39155a601122ac6fd26fb241a18e0ff33e804b96
ssimon23-ai <ssimon23@gmail.com>
2025-03-28 11:43:39 -0700

    Update send_sms.8.x.php

--- quickstart/php/sms/send_sms/send_sms.8.x.php
+++ quickstart/php/sms/send_sms/send_sms.8.x.php
@@ -4,18 +4,18 @@ use Twilio\Rest\Client;
 
 // Your Account SID and Auth Token from twilio.com/console
 // To set up environmental variables, see http://twil.io/secure
-$account_sid = getenv('TWILIO_ACCOUNT_SID');
-$auth_token = getenv('TWILIO_AUTH_TOKEN');
+$account_sid = getenv('AC3fa2f354ff8fa846b835809038e56e52');
+$auth_token = getenv('cb960f6fa34f97d840e38967a08bb311');
 // In production, these should be environment variables. E.g.:
 // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
 
 // A Twilio number you own with SMS capabilities
-$twilio_number = "+15017122661";
+$twilio_number = "+17024470246";
 
 $client = new Client($account_sid, $auth_token);
 $client->messages->create(
     // Where to send a text message (your cell phone?)
-    '+15558675310',
+    '+‪17026259225‬',
     array(
         'from' => $twilio_number,
         'body' => 'I sent this message in under 10 minutes!'
